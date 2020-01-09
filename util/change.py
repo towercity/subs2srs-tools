a9f858c42c601b0de0f43782ddb90caa234e1c86
Matt Nerger <matthewjnerger3@gmail.com>
2020-01-09 18:33:06 -0500

    Set file strucutre

--- util/change.py
+++ util/change.py
@@ -7,25 +7,17 @@ from aqt.qt import *
 import urllib.request as request
 import json
 
-from .util.change import change_decks
-
 JISHO_API = "https://jisho.org/api/v1/search/words?keyword="
-
+# load config files
 config = mw.addonManager.getConfig(__name__)
 
 def search_jisho(word): 
     url = "%s%s" % (JISHO_API, word)
     return json.loads(request.urlopen(url).read())
 
-#def change_decks():
-#    change_tag = config['tags']['change']
-#    change_notes = mw.col.findNotes('note:subs2srs tag:%s' % change_tag) #saves a list of note IDs
-#    showInfo("found %s cards tagged '%s'" % (len(change_notes), change_tag))
+def change_decks():
+    change_tag = config['tags']['change']
+    change_notes = mw.col.findNotes('note:subs2srs tag:%s' % change_tag) #saves a list of note IDs
+    showInfo("found %s cards tagged '%s'" % (len(change_notes), change_tag))
 #    test = search_jisho('red')
 #    showInfo(str(test['meta']['status']))
-
-
-# add menu item to menu   
-action = QAction("Change Decks", mw)
-action.triggered.connect(change_decks)
-mw.form.menuTools.addAction(action)
