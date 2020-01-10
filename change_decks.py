0ad8fec78b90a08e3184e57d9eb0c52d3ce2dfa8
Matt Nerger <matthewjnerger3@gmail.com>
2020-01-09 20:55:35 -0500

    add test wrapper

--- change_decks.py
+++ change_decks.py
@@ -1,3 +1,4 @@
+from aqt.utils import showInfo, getOnlyText
 # import the main window object (mw) from aqt
 from aqt import mw
 # import all of the Qt GUI library
@@ -6,6 +7,14 @@ from aqt.qt import *
 from .util.change import change_decks
 
 # add menu item to menu   
-action = QAction("Change Decks", mw)
-action.triggered.connect(change_decks)
-mw.form.menuTools.addAction(action)
+change = QAction("Change Decks", mw)
+change.triggered.connect(change_decks)
+mw.form.menuTools.addAction(change)
+
+def test_ask():
+    vari = getOnlyText('enter text')
+    showInfo(vari)
+
+add = QAction("test", mw)
+add.triggered.connect(test_ask)
+mw.form.menuTools.addAction(add)
