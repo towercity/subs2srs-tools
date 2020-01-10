a2b78a9f1b563fcff6c0627a51c94bf44b3edbb9
Matt Nerger <matthewjnerger3@gmail.com>
2020-01-09 21:02:46 -0500

    shift toqt style

--- util/add_cards.py
+++ util/add_cards.py
@@ -1,6 +1,12 @@
 import sys
+from aqt import mw
+from aqt.utils import showInfo, getOnlyText, askUser
+
 from .jisho import JishoHandler
 from .change_cards import change_cards
+
+# load config files
+config = mw.addonManager.getConfig(__name__)
 
 jisho = JishoHandler()
 
@@ -14,14 +20,14 @@ def find_root(term, pos):
         
     return ''.join(term)
 
-def add_term(jisho_resp, col, tag, config):
+def add_term(jisho_resp, tag):
     term = jisho.get_japanese_term(jisho_resp)
 
     # check if not already exists; if so, add and leave
-    note_exists = col.findNotes(f"Vocabulary:{term}")
+    note_exists = mw.col.findNotes(f"Vocabulary:{term}")
     if note_exists:
         print(f"{term} already exists in database. Adding tag {tag}...")
-        col.tags.bulkAdd(note_exists, tag, True) #the actual adding logic for anki 
+        mw.col.tags.bulkAdd(note_exists, tag, True) #the actual adding logic for anki 
         return True
 
     print(f"{term} does not yet exist")
@@ -29,28 +35,28 @@ def add_term(jisho_resp, col, tag, config):
     # find root term for better quality searching
     term_root = find_root(term, jisho.get_pos(jisho_resp))
     print('Searching for notes in database with term...')
-    subs2srs_notes = col.findNotes(f"note:{config['models']['subs2srs']} {term_root}")
+    subs2srs_notes = mw.col.findNotes(f"note:{config['models']['subs2srs']} {term_root}")
 
     if subs2srs_notes: #if it's found something....
         print('Note found!\nPreparing note')
         edit_notes = subs2srs_notes[0:1]
-        note = col.getNote(subs2srs_notes[0]) #only edit the first found note
+        note = mw.col.getNote(subs2srs_notes[0]) #only edit the first found note
         note.fields[5] = term #saves the term to the correct field in the model
-        col.tags.bulkAdd(edit_notes, config["tags"]["change"], True) #mark it to change 
-        col.tags.bulkAdd(edit_notes, tag, True) #add the new tag
+        mw.col.tags.bulkAdd(edit_notes, config["tags"]["change"], True) #mark it to change 
+        mw.col.tags.bulkAdd(edit_notes, tag, True) #add the new tag
         note.flush()
     else:
         print('No notes found.\nAdding new card...')
 
         # Set the model
-        modelBasic = col.models.byName(config["models"]["japanese"])
-        col.decks.current()['mid'] = modelBasic['id']
+        modelBasic = mw.col.models.byName(config["models"]["japanese"])
+        mw.col.decks.current()['mid'] = modelBasic['id']
 
         # Get the deck
-        deck = col.decks.byName(config["decks"]["main"])
+        deck = mw.col.decks.byName(config["decks"]["main"])
 
         # Instantiate the new note
-        note = col.newNote()
+        note = mw.col.newNote()
         note.model()['did'] = deck['id']
 
         # Add the fields
@@ -59,15 +65,15 @@ def add_term(jisho_resp, col, tag, config):
         note.fields[3] = jisho.get_definition(jisho_resp) 
 
         # Set the tags (and add the new ones to the deck configuration
-        note.tags = col.tags.canonify(col.tags.split(tag))
+        note.tags = mw.col.tags.canonify(mw.col.tags.split(tag))
         m = note.model()
         m['tags'] = note.tags
-        col.models.save(m)
+        mw.col.models.save(m)
 
         # Add the note
-        col.addNote(note)
+        mw.col.addNote(note)
 
-def add_cards(col, config, tag, new_terms=[]):
+def add_cards(tag, new_terms=[]):
     vocab_archive = [] #keeps record of added cards
 
     if new_terms:
@@ -76,10 +82,10 @@ def add_cards(col, config, tag, new_terms=[]):
         # add a call to the card add function to the new_terms here
         for term in new_terms:
             jisho_resp = jisho.get_term_one(term)
-            if not jisho_resp:
+            if not jisho_resp:``
                 print(f"\"{term}\" not found.")
             else: 
-                add_term(jisho_resp, col, tag, config)
+                add_term(jisho_resp, tag)
                 vocab_archive.append(term)
     else:
         print(f"adding new cards to {tag}")
@@ -87,10 +93,10 @@ def add_cards(col, config, tag, new_terms=[]):
     # start notes add loop
     searching = True
     while searching:
-        term = input("Enter term or 'q' to quit: ")
+        term = getOnlyText(f"Tag: {tag}\nEnter term: ")
 
         # exit condition
-        if term == 'q':
+        if term == 'q' or term == '':
             print('exiting...')
             searching = False
             break
@@ -98,31 +104,26 @@ def add_cards(col, config, tag, new_terms=[]):
         # pull data from jisho        
         jisho_resp = jisho.get_term_one(term)
         if not jisho_resp:
-            print('No term found. rerunning search')
+            showInfo('No term found. rerunning search')
             print(" ------ ")
         else:
             term = jisho.get_japanese_term(jisho_resp)
 
-            print(" ------ ")
-            print(f"Selected Term: {jisho.get_reading(jisho_resp)}")
-            print(f"Part of Speech: {jisho.get_pos(jisho_resp)}")
-            print(f"Definition: {jisho.get_definition(jisho_resp)}")
-            print(" ------ ")
-            add_note = input("Add term? (Y/n): ")
+            add_note = askUser(f"Selected Term: {jisho.get_reading(jisho_resp)}\nPart of Speech: {jisho.get_pos(jisho_resp)}\nDefinition: {jisho.get_definition(jisho_resp)}\n\nAdd term?")
 
-            # take in no answer as yes
-            if add_note == '':
-                add_note = 'y'
-            
-            if add_note == 'y' or add_note == 'Y' or add_note == 'yes' or add_note == 'YES' or add_note == 'Yes':
-                add_term(jisho_resp, col, tag, config) #the logic to add the cardo
-                print(f"added {term}")
+            if add_note == True:
+                add_term(jisho_resp, tag) #the logic to add the cardo
+                showInfo(f"added {term}")
                 vocab_archive.append(term)            
 
         print(f"current archive: {' '.join(vocab_archive)}")
 
     print('Copying over subs2srs notes...')
-    change_cards(col, config)
+    change_cards()
 
     print('saving to database')
-    col.save()
+    mw.col.save()
+
+def add_by_tag():
+    tag = getOnlyText("Enter tag")
+    add_cards(tag)
