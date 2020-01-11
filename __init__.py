9e4e676bea13051336f5704f98ee50c96f6683f8
Matt Nerger <matthewjnerger3@gmail.com>
2020-01-11 15:58:13 -0500

    clarity commetns

--- __init__.py
+++ __init__.py
@@ -1,19 +1,20 @@
+# Anki imports
 from aqt.utils import showInfo, getOnlyText
-# import the main window object (mw) from aqt
 from aqt import mw
-# import all of the Qt GUI library
 from aqt.qt import *
 
+# local functions import
 from .util.change import change_decks
 from .util.add_cards import add_by_tag
 
-# add menu item to menu   
+# add change function to menus   
 change = QAction("Change subs2srs cards", mw)
 change.triggered.connect(change_decks)
 change.setShortcut('c')
 mw.form.menuTools.addAction(change)
 
-add = QAction("Add Cards to Tag", mw)
+# add add function to menus
+add = QAction("Add to tag", mw)
 add.triggered.connect(add_by_tag)
 add.setShortcut('Shift+A')
 mw.form.menuTools.addAction(add)
