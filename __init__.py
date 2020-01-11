4207d70b8bf3d62f49ec86b086d7249d01040c11
Matt Nerger <matthewjnerger3@gmail.com>
2020-01-11 15:55:42 -0500

    rename functions

--- __init__.py
+++ __init__.py
@@ -8,16 +8,12 @@ from .util.change import change_decks
 from .util.add_cards import add_by_tag
 
 # add menu item to menu   
-change = QAction("Change Decks", mw)
+change = QAction("Change subs2srs cards", mw)
 change.triggered.connect(change_decks)
 change.setShortcut('c')
 mw.form.menuTools.addAction(change)
 
-def test_ask():
-    vari = getOnlyText('enter text')
-    showInfo(vari)
-
-add = QAction("Add Cards", mw)
+add = QAction("Add Cards to Tag", mw)
 add.triggered.connect(add_by_tag)
 add.setShortcut('Shift+A')
 mw.form.menuTools.addAction(add)
