2fc2fbe38176d113dba3df420a67db622ade8c48
Matt Nerger <matthewjnerger3@gmail.com>
2020-01-11 16:00:11 -0500

    make shortcuts configureable

--- __init__.py
+++ __init__.py
@@ -7,14 +7,17 @@ from aqt.qt import *
 from .util.change import change_decks
 from .util.add_cards import add_by_tag
 
+# load config files
+config = mw.addonManager.getConfig(__name__)
+
 # add change function to menus   
 change = QAction("Change subs2srs cards", mw)
 change.triggered.connect(change_decks)
-change.setShortcut('c')
+change.setShortcut(config['shortcuts']['change'])
 mw.form.menuTools.addAction(change)
 
 # add add function to menus
 add = QAction("Add to tag", mw)
 add.triggered.connect(add_by_tag)
-add.setShortcut('Shift+A')
+add.setShortcut(config['shortcuts']['add'])
 mw.form.menuTools.addAction(add)
